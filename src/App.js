8ec96aa314335d56b25ca1718c45169fc486eab7
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-09-29 08:42:58 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import Component from 'inferno-component';
 import './registerServiceWorker';
 import Logo from './logo';
 import './App.css';
-import NameForm from './NameForm'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <Logo width="80" height="80" />
-          <h2>{`Welcome to Inferno ${version}`}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NameForm />
+const App = ({ children }) => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Logo width="80" height="80" />
+        <h2>{`Welcome to Inferno ${version}`}</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      { children }
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import { render } from 'inferno';
+import { Router, Route, IndexRoute } from 'inferno-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
 import './index.css';
 
-render(<App />, document.getElementById('app'));
+const Home = (props, cb) => import ('./Home').then(component => cb(null, component.default))
+const NoMatch = (props, cb) => import ('./NoMatch').then(component => cb(null, component.default))
+
+const browserHistory = createBrowserHistory();
+
+render(
+  <Router history={browserHistory}>
+    <Route component={App}>
+      <IndexRoute getComponent={Home} />
+      <Route path="*" getComponent={NoMatch} />
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
