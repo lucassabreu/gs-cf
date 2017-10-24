79de5f13b5afdd10b1c12f6050ea1bdf61b50af2
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-10-24 21:46:43 -0200

    Menu wip

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { matchPath } from 'react-router'
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types';
