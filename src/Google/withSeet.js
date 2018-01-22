9e9b3c75cdfb6e13215e928d775c136496f3f2e2
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-01-22 08:22:06 -0200

    no props

--- src/Google/withSeet.js
+++ src/Google/withSeet.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import getDisplayName from './getDisplayName'
+import getDisplayName from '../getDisplayName'
 
 const withSeet = (getSheet) => (WrappedComponent) => {
   const WithSeet = ({ match, ...restProps }) => {
     let sheet = null;
-    if (!props.match.params.sheetId) {
-      sheet = getSheet(props.match.params.sheetId);
+    if (!match.params.sheetId) {
+      sheet = getSheet(match.params.sheetId);
     }
 
     return <WrappedComponent sheet={sheet} {...restProps} />
