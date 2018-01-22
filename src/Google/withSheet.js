9bff88d65fea5c1653d34a001ddbc5d9ceb7dda3
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-01-22 08:36:33 -0200

    withSeet => withSheet

--- src/Google/withSheet.js
+++ src/Google/withSheet.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import getDisplayName from '../getDisplayName'
 
-const withSeet = (getSheet) => (WrappedComponent) => {
-  const WithSeet = ({ match, ...restProps }) => {
+const withSheet = (getSheet) => (WrappedComponent) => {
+  const WithSheet = ({ match, ...restProps }) => {
     let sheet = null;
     if (!match.params.sheetId) {
       sheet = getSheet(match.params.sheetId);
@@ -10,8 +10,8 @@ const withSeet = (getSheet) => (WrappedComponent) => {
 
     return <WrappedComponent sheet={sheet} {...restProps} />
   }
-  WithSeet.displayName = `WithSheet(${getDisplayName(WrappedComponent)})`
-  return WithSeet;
+  WithSheet.displayName = `WithSheet(${getDisplayName(WrappedComponent)})`
+  return WithSheet;
 }
 
-export default withSeet;
+export default withSheet;
