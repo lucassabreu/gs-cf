6a3c6ec3cbc8ae23a9c1e9dfe0f1133385fbf375
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-01-24 21:14:42 -0200

    getMonths

--- src/Google/Sheet.js
+++ src/Google/Sheet.js
@@ -3,11 +3,13 @@
 import GoogleAPIService from './GoogleAPIService'
 import promisify from './promisify'
 import Movement from '../Model/Movement'
+import Month from '../Model/Month'
 
 export default class Sheet {
   id = null;
   googleAPIService = null
   _allMovements = null
+  _months = null;
 
   /**
    * @param {string} id
@@ -40,7 +42,7 @@ export default class Sheet {
     }));
 
     let rows = data.result.values.slice(1);
-    let moviments = [];
+    let movements = [];
     for (var index in rows) {
       let row = rows[index];
       let value = parseFloat(row[3].replace(/[^-0-9.]/g, ''));
@@ -52,14 +54,14 @@ export default class Sheet {
       let dateParts = row[0].split('/');
       let date = new Date(dateParts[2], (dateParts[1] - 1), dateParts[0]);
 
-      moviments.push(new Movement(index + 1, date, row[1], row[2], value, row[4]));
+      movements.push(new Movement(index + 1, date, row[1], row[2], value, row[4]));
     }
 
-    return this._allMovements = moviments;
+    return this._allMovements = movements;
   }
 
   /**
-   * Get all Moviments from a filter
+   * Get all Movements from a filter
    * @param {Object} filter
    * @param {Date} filter.month Month to filter
    * @param {Date} filter.start Start nonth to filter
@@ -97,6 +99,90 @@ export default class Sheet {
     return movements
   }
 
+  async getAllMonths() {
+    if (this._months != null) {
+      return this._months;
+    }
+
+    const dateToKey = (date) => parseInt(
+      date.toISOString()
+        .substring(0, 6)
+        .replace(/[^0-9]/g, ''),
+      10
+    );
+
+    const movements = await this.getAll();
+    let months = movements
+      .reduce((months, movement) => {
+        const key = dateToKey(movement.date);
+        if (!months[key]) {
+          months[key] = new Month({
+            index: key,
+            month: movement.getMonth(),
+          })
+        }
+
+        months[key]
+          .addMovement(movement);
+        return months;
+      }, [])
+      .map((month, index, months) => {
+        let nextMonth = new Date(month.month);
+        nextMonth.setMonth(month.month.getMonth() + 1)
+
+        let prevMonth = new Date(month.month);
+        prevMonth.setMonth(month.month.getMonth() - 1)
+
+        const nextIndex = dateToKey(nextMonth);
+        const prevIndex = dateToKey(prevMonth);
+
+        month.next = months[nextIndex];
+        month.prev = months[prevIndex];
+
+        return month;
+      });
+
+    return this._months = months;
+  }
+
+  /**
+   * Get all Months from a filter
+   * @param {Object} filter
+   * @param {Date} filter.month Month to filter
+   * @param {Date} filter.start Start nonth to filter
+   * @param {Date} filter.end End month to filter
+   * @returns {Month[]}
+   */
+  async getMonths({ month, start, end }) {
+    let months = await this.getAllMonths();
+
+    if (month) {
+      return months.filter(m => (
+        m.date.getFullYear() === month.getFullYear() &&
+        m.date.getMonth() === month.getMonth()
+      ));
+    }
+
+    if (start) {
+      start = new Date(start);
+      start.setDate(1)
+      months = months.filter((m) => {
+        return m.month >= start;
+      })
+    }
+
+    if (end) {
+      end = new Date(end);
+      end.setMonth(end.getMonth() + 1)
+      end.setDate(-1)
+      months = months.filter((m) => {
+        return m.month <= end;
+      })
+    }
+
+    return months;
+  }
+
   /**
    * @param {Date} date
    * @returns {Number}
@@ -110,7 +196,7 @@ export default class Sheet {
    * @param {Date} date
    * @returns {Number}
    */
-  async getTotalMovimentAt(date) {
+  async getTotalMovementAt(date) {
     let movements = await this.getMovements({ end: date })
     return movements.reduce((prev, movement) => prev + Math.asb(movement.getValue()), 0);
   }
--- src/Model/Month.js
+++ src/Model/Month.js
@@ -1,6 +1,13 @@
-import Moviment from './Movement'
-
 export default class Month {
+  month = null;
+  index = null;
+  balance = null;
+  credit = null;
+  debit = null;
+  movements = null;
+  prev = null;
+  next = null;
+
   /**
    * @param {Object} param
    * @param {*} param.index
@@ -8,14 +15,26 @@ export default class Month {
    * @param {Number} param.balance
    * @param {Number} param.credit
    * @param {Number} param.debit
-   * @param {Moviment[]} param.moviments
+   * @param {Movement[]} param.movements
    */
-  constructor({ index, month, balance, credit, debit, moviments }) {
+  constructor({ index, month, balance, credit, debit, movements }) {
     this.month = month;
     this.index = index;
-    this.balance = balance;
-    this.credit = credit;
-    this.debit = debit;
-    this.moviments = moviments;
+    this.balance = balance || 0;
+    this.credit = credit || 0;
+    this.debit = debit || 0;
+    this.movements = movements || [];
+  }
+
+  /**
+   * @param {Movement} movement
+   * @return {Month} this
+   */
+  addMovement(movement) {
+    this[movement.value > 0 ? 'credit' : 'debit'] += movement.value;
+    this.balance += movement.value;
+    this.movements.push(movement);
+
+    return this;
   }
 }
