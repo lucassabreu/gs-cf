b04f973abf87c711019eaf0d291a7c575c3bf2d0
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2018-01-27 10:24:42 -0200

    Menu => {Navbar, SheetMenu, UserMenu}

---
+++ src/Menu/UserMenu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const UserMenu = ({ user, className }) => (
+  <ul className={`navbar-nav ${className}`}>
+    <li className="nav-item hidden-sm">
+      <span className="nav-link">{user.name}</span>
+    </li>
+    <li className="nav-item">
+      <span className="nav-link" style={{ paddingBottom: 0, paddingTop: ".3rem" }}>
+        <img src={user.image} width="30" height="30" alt="" />
+      </span>
+    </li>
+    <li className="nav-item">
+      <Link className="btn nav-link" to='/logout'>Logout</Link>
+    </li>
+  </ul>
+)
+
+export default UserMenu
